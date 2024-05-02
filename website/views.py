91e6279f340a430781b7c508be932e18a3ce2126
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-02 21:25:39 +0530

    create updating records

--- website/views.py
+++ website/views.py
@@ -53,10 +53,10 @@ def delete_record(request, pk):
     if request.user.is_authenticated:
         delete_it = Record.objects.get(id=pk)
         delete_it.delete()
-        messages.success(request, "Record Deleted Successfully!!!")
+        messages.success(request, "Record deleted successfully!!!")
         return redirect('home')
     else:
-        messages.success(request, "You Must Be Logged In To Do That!!!")
+        messages.success(request, "You must be logged in to do that!!!")
         return redirect('home')
     
 def add_record(request):
@@ -65,9 +65,22 @@ def add_record(request):
         if request.method == "POST":
             if form.is_valid():
                 form.save()
-                messages.success(request, "Record Added!!!")
+                messages.success(request, "Record added!!!")
                 return redirect('home')
         return render(request, 'add_record.html', {'form': form})
     else:
-        messages.success(request, "You Must Be Logged In!!!")
+        messages.success(request, "You must be logged in!!!")
+        return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record has been updated!!!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form, 'customer_record': current_record})
+    else:
+        messages.success(request, "You must be logged in!!!")
         return redirect('home')
